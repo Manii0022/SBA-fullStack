b84a00d45983392b6ad1d83e3f3dd689b414e005
Manish <manishlodhi0224@gmail.com>
2025-08-20 15:24:45 +0530

    react setup

---
+++ FullStack_Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-amber-300 h-[400px] w-[400px] '>
+            <h1 className='bg-amber-200 text-3xl p-2 m-2'> Hello manish</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
