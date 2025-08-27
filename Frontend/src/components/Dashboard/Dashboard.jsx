10f79d2c631d0df50a020670ee1cffedc87bd9e5
Manish <manishlodhi0224@gmail.com>
2025-08-27 23:04:28 +0530

    show user data

--- Frontend/src/components/Dashboard/Dashboard.jsx
+++ Frontend/src/components/Dashboard/Dashboard.jsx
@@ -6,12 +6,10 @@ function Dashboard() {
 
     const { setShowHeader } = useLayout();
     useEffect(() => {
-        setShowHeader(false);
+        setShowHeader(true);
     }, [])
 
     const [data, setData] = useState(null);
-
-
     function handleClick() {
         console.log("button clicked");
         fetch("https://reqres.in/api/users?page=2", {
@@ -27,37 +25,48 @@ function Dashboard() {
             .catch(error => console.log(`Cannot fetch data ${error}`));
     }
 
-     const [user, setUser] = useState(null);
-        const [token, setToken] = useState(null);
-        const location = useLocation();
-    
-        useEffect(() => {
-            const query = new URLSearchParams(location.search);
-            const token = query.get("token");
-            setToken(token);
-            const email = query.get("email");
-            const name = query.get("name");
-            const exists = query.get("exists");
-    
-            if (token) {
-                localStorage.setItem("token", token);
-                setUser({ email, name, newUser: true });
-            } else if (exists) {
-                setUser({ email, name, newUser: false });
-            }
-        }, [location.search]);
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+    const location = useLocation();
+
+    useEffect(() => {
+        const query = new URLSearchParams(location.search);
+        const token = query.get("token");
+        setToken(token);
+        const email = query.get("email");
+        const name = query.get("name");
+        const exists = query.get("exists");
+
+        if (token) {
+            localStorage.setItem("token", token);
+            setUser({ email, name, newUser: true });
+        } else if (exists) {
+            setUser({ email, name, newUser: false });
+        }
+    }, [location.search]);
 
 
     return (
         <div className="font-serif ml-10 mr-10 mt-10 ">
-            <div className="bg-cyan-200 flex justify-around ">
-                <div>
+            <div className="bg-cyan-200 flex justify-between ">
+                <div className="ml-5">
                     <img className="size-[150px]" src="https://images.unsplash.com/vector-1738925655186-02fbc43662cf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGpvdXJuYWx8ZW58MHx8MHx8fDA%3D" alt="logo" />
                     <span className="text-3xl ">Basic Journal Dashboard</span>
                 </div>
-                <div className="flex justify-center items-center ">
+                <div className="flex-col justify-center items-center mr-5">
                     <span className="text-8xl ">
                         Your Daily Journal
+                    </span>
+                    <span>
+                        {user ? (
+                            <div className="text-3xl ml-10 mt-5">
+                                {`Welcome, ${user.name} !` }
+                            </div>
+                        ) : (
+                            <div className="text-3xl ml-10 mt-5">
+                                Welcome to your journal dashboard!
+                            </div>
+                        )}
                     </span>
                 </div>
 
