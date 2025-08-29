5311674d8fe2bd157ef4766bc069201900423465
Manish <manishlodhi0224@gmail.com>
2025-08-29 22:00:37 +0530

    default header in dashboard

---
+++ Frontend/src/components/Dashboard/Default.jsx
@@ -0,0 +1,9 @@
+function Default() {
+    return (
+        <div className="mt-5 mb-5 text-6xl font-serif h-[50vh]">
+            <h1 className="font-bold underline">Select any of the above options </h1>
+        </div>
+    );
+}
+
+export default Default;
