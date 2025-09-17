855428e904bfc39c983aeb328cfd063ca8b6112e
Manish <manishlodhi0224@gmail.com>
2025-09-17 17:47:58 +0530

    added all

--- Frontend/src/components/LandingLayout.jsx
+++ Frontend/src/components/LandingLayout.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import HeaderN from "./Header/Header.jsx";
-import FooterN from "./Footer/Footer.jsx"
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx"
 
 function LandingLayout() {
 
     return (
         <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
             {/* Navigation Header */}
-            <HeaderN />
-
+            <Header />
 
             {/* Main Content */}
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -16,7 +15,7 @@ function LandingLayout() {
             </main>
 
             {/* Footer */}
-            <FooterN />
+            <Footer />
         </div>
     );
 };
